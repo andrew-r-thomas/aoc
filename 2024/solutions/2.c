638a6b1da0f3706365e77e2963107d1b1aa6c4c8
Andrew Thomas <andrew@andrewrthomas.com>
2024-12-04 19:31:50 -0800

    cue the dora we did it music

--- 2024/solutions/2.c
+++ 2024/solutions/2.c
@@ -1,6 +1,10 @@
 /*
  just put the lines in an array and do stuff that way,
  need to do some more complete checking for the removal stuff
+
+ NOTE:
+ friendly reminder, probably want to keep code from part 1 when you
+ implement part 2
 */
 
 #include <stdio.h>
@@ -13,116 +17,121 @@ enum DIRECTION
 	initial,
 };
 
-int check_safe(int prev, int current, enum DIRECTION *dir)
+int line_is_safe(int line[], int len)
 {
-	if (!(prev == 0))
+	enum DIRECTION dir = initial;
+	int current = 0;
+	int previous = 0;
+	for (int i = 0; i < len; i++)
 	{
-		// check diff
-		int diff = abs(current - prev);
-		if (diff < 1 || diff > 3)
-		{
-			return 0;
-		}
+		previous = current;
+		current = line[i];
 
-		// check asc/dec
-		switch (*dir)
+		if (!(previous == 0))
 		{
-			case initial:
-				if (current < prev) *dir = decreasing;
-				else if (current > prev) *dir = increasing;
-				else
-				{
-					return 0;
+			// check prev for violations
+			int diff = abs(current - previous);
+			if (diff < 1 || diff > 3) return 0;
+			else {
+				switch (dir) {
+				case initial:
+					if (current > previous) dir = increasing;
+					else if (current < previous) dir = decreasing;
+					else return 0;
+					break;
+				case increasing:
+					if (!(current > previous)) return 0;
+					break;
+				case decreasing:
+					if (!(current < previous)) return 0;
+					break;
 				}
-				break;
-			case increasing:
-				if (!(current > prev))
-				{
-					return 0;
-				}
-				break;
-			case decreasing:
-				if (!(current < prev))
-				{
-					return 0;
-				}
-				break;
+			}
 		}
 	}
 
 	return 1;
 }
 
-int line_is_safe(char line[])
+int parse_line(char line[], int len, int buff[])
 {
-	int safe = 1;
-	int first = 1;
-
-	enum DIRECTION dir = initial;
-	int prev = 0;
 	int current = 0;
-	int i = 0;
-	int looping = 1;
-	while (looping)
+	int buff_i = 0;
+	for (int i = 0; i < len; i++)
 	{
-		int s;
 		char c = line[i];
 		int digit;
 		switch (c) {
 		case ' ':
-			s = check_safe(prev, current, &dir);
-			if (first == 1 && s == 0)
-			{
-				first = 0;
-			}
-			else
-			{
-				looping = s;
-				safe = s;
-			}
-			prev = current;
+			buff[buff_i] = current;
+			buff_i++;
 			current = 0;
 			break;
+
 		case '\n':
-			// maybe need to add first check here
-			s = check_safe(prev, current, &dir);
-			safe = s;
-			looping = 0;
+			buff[buff_i] = current;
+			buff_i++;
 			break;
+
 		default:
 			digit = c - '0';
 			current *= 10;
 			current += digit;
 			break;
 		}
-
-		i++;
 	}
 
-	return safe;
+	return buff_i;
 }
 
-#define LINES 6
+#define LINES 1000
 
 int main(void)
 {
-	FILE *f = fopen("../data/2.txt", "r");
+	FILE *f = fopen("data/2.txt", "r");
 
 	int answer = 0;
-	for (int i = 0; i < LINES; i++)
+	for (int line_i = 0; line_i < LINES; line_i++)
 	{
 		char line_buff[25] = {0};
 
 		char current = 0;
-		int j = 0;
+		int line_buff_i = 0;
 		while (current != '\n')
 		{
 			fread(&current, 1, 1, f);
-			line_buff[j] = current;
-			j++;
+			line_buff[line_buff_i] = current;
+			line_buff_i++;
 		}
 
-		answer += line_is_safe(line_buff);
+		int num_buff[25] = {0};
+		int len = parse_line(line_buff, line_buff_i, num_buff);
+
+		int safe = line_is_safe(num_buff, len);
+		if (!safe)
+		{
+			for (int rebuff_i = 0; rebuff_i < len; rebuff_i++)
+			{
+				// make a buff without this entry
+				int buff[25] = {0};
+				int b_i = 0;
+				for (int buff_i = 0; buff_i < len; buff_i++)
+				{
+					if (buff_i != rebuff_i)
+					{
+						buff[b_i] = num_buff[buff_i];
+						b_i++;
+					}
+				}
+
+				if (line_is_safe(buff, len - 1))
+				{
+					answer += 1;
+					break;
+				}
+			}
+		}
+		else answer += 1;
 	}
 
 	printf("answer is: %d\n", answer);
